17f62ffe27f4361f60792691c5648dce64e0059a
redxouls <b08901058@ntu.edu.tw>
2019-10-08 14:50:01 +0800

    RLE to Plain text

--- RlEtonorm.py
+++ RlEtonorm.py
@@ -1,31 +1,43 @@
-k = input()
+k = input() + ".txt"
 f = open(k,"r").read()
-print(f)
 output = input("Ouput file name :") + ".txt"
-
+ma = 0
 with open(output,"w") as text_file:
     temp =""
+    count = 0
     for i in range(len(f)):
         try:
             int(f[i])
             temp += f[i]
         except ValueError:
-            print("GG",temp)
+            #if temp != '':
+                #print(temp)
             if temp =="":
                 temp="1"
             if f[i] == "b":
-                print("b")
-                temp = int(temp)
-                a = "O"*temp
-                print(a)
-                text_file.write(a)
-            elif f[i]== "o":
                 temp = int(temp)
                 a = "."*temp
                 text_file.write(a)
+                if temp > ma :
+                    ma = temp
+            elif f[i]== "o":
+                temp = int(temp)
+                a = "O"*temp
+                text_file.write(a)
+                if temp > ma :
+                    ma = temp
             elif f[i] =="$":
+                count += 1
+                #print("newline")
                 text_file.write("\n")
             elif f[i] =="\n":
+                temp = ''
                 continue
+            if temp == 1123:
+                tempp = ''
+                for k in range(-10,10):
+                    tempp += f[i+k]
+                
+                print(tempp)
             temp = ''
-            
+print(count,ma)
